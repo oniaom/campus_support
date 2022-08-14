38427f190003609f005caabfcca9f707415e9896
oniaom <56845674+oniaom@users.noreply.github.com>
2022-08-15 00:22:58 +0300

    Delete campus_support_team directory

--- campus_support_team/run.bat
+++
@@ -1,1 +0,0 @@
-flask --debug run --host=0.0.0.0
--- campus_support_team/static/js/search_box_on_input.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function () {
-    let input = document.querySelector("input");
-    document.getElementById("q").addEventListener("input", async function () {
-        let response = await fetch("/search_plain?q=" + input.value);
-        let database_results = await response.text();
-        document.querySelector("tbody").innerHTML = database_results;
-    });
-
-});
